34a3c85b9354ad0065aa1389badc457274f9993e
pauluZ <pawel.szczepanek@gmail.com>
2015-05-14 14:29:50 +0200

    migration for products

---
+++ application/migrations/005_add_user_field_to_product.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * pZ:
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Add_user_field_to_product extends CI_Migration {
+
+	public function up()
+	{
+		$col = array(
+			'user_id' => array(
+				'type' => 'INT',
+				'constraint' => 11
+			)
+		);
+
+		$this->dbforge->add_column('ip_products', $col , 'product_id');
+
+		log_message('info', 'Deskdoo Migration: ' . get_class());
+	}
+
+	public function down()
+	{
+	}
+}
